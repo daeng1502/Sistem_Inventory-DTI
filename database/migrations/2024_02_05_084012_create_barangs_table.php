982c48b5c380ee49fb4f422a3be21b72243dc22a
FathihAlfi <91770998+FathihAlfi@users.noreply.github.com>
2024-02-06 11:15:37 +0700

    tambah tabel barang

---
+++ database/migrations/2024_02_05_084012_create_barangs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->string('SN');
+            $table->string('nama');
+            $table->string('merk');
+            $table->string('spesifikasi');
+            $table->integer('jumlah barang');
+            $table->string('no_kontrak');
+            $table->string('nama_kontrak');
+            $table->date('tgl_kontrak');
+            $table->string('lokasi');
+            $table->integer('tahun_perolehan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
