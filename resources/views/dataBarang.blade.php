680c915af5a2ef8be59fe9ea0122d132f95489dd
FathihAlfi <91770998+FathihAlfi@users.noreply.github.com>
2024-02-18 22:22:14 +0700

    CRUD DONE!

--- resources/views/dataBarang.blade.php
+++ resources/views/dataBarang.blade.php
@@ -14,22 +14,33 @@
     
     <!-- Your HTML layout -->
 <div class="container">
-    <div class="position-relative">
-        <a href="/tambahBarang" class="btn btn-primary position-absolute bottom-0 end-0 mt-4">Tambah Barang</a>
+    <div class="position-relative mb-3" >
+        <a href="/tambahBarang" class="btn btn-primary position-absolute bottom-0 end-0 mt-4"> + Tambah Barang</a>
+        
+        <div class="row g-3 align-items-center">
 
-    <div class="row g-3 align-items-center">
-            <div class="col-auto">
-                <label for="inputPassword6" class="col-form-label">SEARCH ITEM : </label>
-        </div>
         <div class="col-auto">
-            <form action="/ManajemenItem" method="GET">
-                <input type="search" name="search" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
-            </form>
+        <div class="input-group md-form form-sm form-1 pl-0">
+        <div class="input-group-prepend">
+            <span class="input-group-text cyan lighten-2" id="basic-text1"><i class="fas fa-search text-white"
+                aria-hidden="true"></i><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+            </svg></span>
         </div>
+        <form action="/ManajemenItem" method="GET" class="d-inline">
+        <input class="form-control my-0 py-1" type="search" name="search" placeholder="Search" aria-label="Search">
+        </div>
+        </div>
+
+            <!-- <div class="col-auto">
+                <form action="/ManajemenItem" method="GET" class="d-inline">
+                    <input type="search" name="search" id="inputPassword6" class="form-control d-inline" aria-describedby="passwordHelpInline" placeholder="SEARCH ITEM">
+                </form>
+</div> -->
         <div class="col-auto">
         </div>
     </div>
-    </div>
+</div>
         <div class="row">
         <!-- @if($message = Session::get('success'))
             <div class="alert alert-success" role="alert">
@@ -45,11 +56,12 @@
                     <th scope="col">Merk</th>
                     <th scope="col">Spesifikasi</th>
                     <th scope="col">Jumlah</th>
-                    <th scope="col">No Kontak</th>
+                    <!-- <th scope="col">No Kontak</th>
                     <th scope="col">Nama Kontrak</th>
-                    <th scope="col">Tanggal Kontrak</th>
+                    <th scope="col">Tanggal Kontrak</th> -->
                     <th scope="col">Lokasi</th>
-                    <th scope="col">Tanggal Perolehan</th>
+                    <th scope="col">Tahun Perolehan</th>
+                    <!-- <th scope="col">QR Code</th> -->
                     <th scope="col">Aksi</th>
                 </tr>
             </thead>
@@ -65,14 +77,27 @@
                     <td>{{ $row->merk }}</td>
                     <td>{{ $row->spesifikasi }}</td>
                     <td>{{ $row->jumlah_barang }}</td>
-                    <td>{{ $row->no_kontrak }}</td>
+                    <!-- <td>{{ $row->no_kontrak }}</td>
                     <td>{{ $row->nama_kontrak }}</td>
-                    <td>{{ $row->tgl_kontrak }}</td>
+                    <td>{{ $row->tgl_kontrak }}</td> -->
                     <td>{{ $row->lokasi }}</td>
                     <td>{{ $row->tahun_perolehan }}</td>
                     <td>
-                        <a href="/tampilBarang/{{ $row->SN }}"  class="btn btn-info">Edit</a>
-                        <a href="#" class="btn btn-danger delete" data-id="{{ $row->SN }}" data-id="{{ $row->nama }}">Delete</a>
+                        <a href="/tampilBarang/{{ $row->SN }}"  class="btn btn-info">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                            </svg>
+                        </a>
+
+                        <a href="#" class="btn btn-danger delete" data-id="{{ $row->SN }}" data-id="{{ $row->nama }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                            </svg>
+                        </a>
+
+
+
                     </td>
                 </tr>
                 @endforeach
