111895cc8e802deecfe49f418f000026f92f8c47
daeng1502 <daengfebrino01@gmail.com>
2024-02-19 15:55:56 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        if(Auth::id())
+        {
+            $usertype=Auth()->user()->usertype;
+            if($usertype=='user')
+            {
+                return view('userdashboard');
+            }
 
+            else if($usertype=='admin')
+            {
+                return view('dashboard');
+            }
+        }
     }
 }
