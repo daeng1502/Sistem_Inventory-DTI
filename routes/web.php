9b082b8785378e317a6d876eb1d9f21058c38979
faiz <faizabdullah2708@gmail,com>
2024-02-19 13:07:31 +0700

    request maintenance

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/requestMaintenance', function () {
+    return view('requestMaintenance');
+});
